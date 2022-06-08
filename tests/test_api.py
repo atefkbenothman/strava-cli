520273a834cebc970cc267f79b82b261b23dc2b7
atefkbenothman <atefkbenothman@gmail.com>
2022-06-08 12:16:47 -0700

    re-structured project

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,4 @@
-from .api import API
+from src.strava.api import API
 
 TEST_BASE_URL = "https://test.com"
 TEST_API_ENDPOINT = "athelete/activities"
@@ -9,6 +9,7 @@ def test_can_generate_url():
   """
   api = API()
   generated_url = api.generate_url(TEST_API_ENDPOINT)
+  print(f"{generated_url=}")
   assert "https://www.strava.com/api/v3/athelete/activities" == generated_url
 
 def test_can_generate_url_custom_base_url():
@@ -18,4 +19,5 @@ def test_can_generate_url_custom_base_url():
   """
   api = API(base_url=TEST_BASE_URL)
   generated_url = api.generate_url(TEST_API_ENDPOINT)
+  print(f"{generated_url=}")
   assert "https://test.com/athelete/activities" == generated_url
