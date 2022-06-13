1154ff6159ea329a46b845273451237fb390610e
atefkbenothman <atefkbenothman@gmail.com>
2022-06-12 18:58:09 -0700

    refactor tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,14 +3,16 @@ from strava.api import API
 TEST_BASE_URL = "https://test.com"
 TEST_API_ENDPOINT = "athelete/activities"
 
+
 def test_can_generate_url():
   """
   Test that the api class can generate the correct url.
   """
   api = API()
-  generated_url = api.generate_url(TEST_API_ENDPOINT)
+  generated_url = api._generate_url(TEST_API_ENDPOINT)
   print(f"{generated_url=}")
   assert "https://www.strava.com/api/v3/athelete/activities" == generated_url
+
 
 def test_can_generate_url_custom_base_url():
   """
@@ -18,6 +20,6 @@ def test_can_generate_url_custom_base_url():
   using a custom base url.
   """
   api = API(base_url=TEST_BASE_URL)
-  generated_url = api.generate_url(TEST_API_ENDPOINT)
+  generated_url = api._generate_url(TEST_API_ENDPOINT)
   print(f"{generated_url=}")
   assert "https://test.com/athelete/activities" == generated_url
